0e48089ff24b7a56ede3f37997d5db5a74e23d8d
Dayrook <derekscherer@gmail.com>
2022-09-18 23:30:00 -0500

    Commented out shader.txt debug

--- addons/zylann.sdf_blender/sdf_container.gd
+++ addons/zylann.sdf_blender/sdf_container.gd
@@ -31,6 +31,7 @@ func _ready():
 	mesh = pm
 	
 	set_process(true)
+	_update_shader()
 
 
 func set_object_param(so, param_index: int, value):
@@ -82,7 +83,7 @@ func _update_shader():
 
 	var code := _generate_shader_code(_objects, _shader_template)
 	# This is for debugging
-	_debug_dump_text_file("generated_shader.txt", code)
+	#_debug_dump_text_file("generated_shader.txt", code)
 
 	shader.code = code
 	_shader_material.set_shader(shader)
